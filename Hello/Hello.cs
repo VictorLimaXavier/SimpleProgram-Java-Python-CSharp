e690746b76756bbd235dbfb3689e3997363c9c12
Discivo <victor.lima.xavier@hotmail.com>
2020-06-26 13:14:09 -0300

    test

--- Hello/Hello.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Hello{
-    public static void Main(string [] args){
-        Console.WriteLine("What is your name?");
-        string name = Console.ReadLine();
-        Console.WriteLine("Hello " + name);
-    }
-}
